c9c3921270d756cf9a5f822df76116f56dc01f00
MatveyTolstov <isip_m.a.tolstov@mpt.ru>
2023-09-21 12:07:33 +0300

    Create прямоуголньник.py

---
+++ прямоуголньник.py
@@ -0,0 +1,7 @@
+heigh = float (input ("Введите высоту"))
+width = float (input ("Введите ширину"))
+def s (heigh,width):
+    print (heigh * width)
+    return heigh * width
+s (heigh,width)
+print()
